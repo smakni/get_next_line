93fa09fb8c3154362839123f0be3bdb13ebb87a0
Sabri MAKNI <smakni@e2r10p5.42.fr>
2018-05-29 19:32:50 +0200

    add ft : clear_read

---
+++ gcc.sh
@@ -0,0 +1,1 @@
+gcc -fsanitize=address -g3 get_next_line.c libft/libft.a
--- get_next_line.c
+++ get_next_line.c
@@ -6,7 +6,7 @@
 /*   By: smakni <smakni@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2018/05/25 11:15:06 by smakni            #+#    #+#             */
-/*   Updated: 2018/05/29 18:29:48 by smakni           ###   ########.fr       */
+/*   Updated: 2018/05/29 19:28:23 by smakni           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,12 +14,36 @@
 
 int		len_line(char *str)
 {
-	int i;
+	size_t i;
 
 	i = 0;
-	while (str[i] != '\n')
+	while (str[i] && str[i] != '\n')
 		i++;
 	return (i);
+}
+
+static char 	*clear_read(char *str)
+{
+	int 	i;
+	int		j;
+	char 	*new;
+
+	i = 0;
+	j = 0;
+	while (str[i] && str[i] != '\n')
+		i++;
+	new = ft_memalloc(ft_strlen(str) - i);
+	if (str[i] == '\n')
+		i++;
+	while (str[i])
+	{	
+		new[j] = str[i];
+		i++;
+		j++;
+	}
+	new[j] = '\0';
+	ft_strdel(&str);
+	return (new);
 }
 
 int		get_next_line(const int fd, char **line)
@@ -30,34 +54,35 @@ int		get_next_line(const int fd, char **line)
 
 	if (!tmp)
 		tmp = ft_memalloc(1);
-	while ((ret = read(fd, buffer, BUFF_SIZE)))
+	while ((ret = read(fd, buffer, BUFF_SIZE)) > 0)
 	{
 		buffer[ret] = '\0';
 		tmp = ft_strjoin(tmp, buffer);
-		if (ft_memchr(buffer, '\n', BUFF_SIZE) != 0)
+		if (ft_memchr(buffer, '\n', ret) != 0)
 			break ;
 	}
-	*line = ft_strtrim(ft_strdup((const char *)tmp));
-	ft_strdel(&tmp);
-	if (ft_memchr(buffer, '\n', BUFF_SIZE) != 0)
-		return (1);
-	if (ret == 0)
+	*line = ft_memalloc(len_line(tmp) + 1);
+	*line = ft_strncpy(*line, (const char *)tmp, len_line(tmp));
+	tmp = clear_read(tmp);
+	if (ret < BUFF_SIZE && !ft_strlen(tmp))
 		return (0);
-	return (-1);
+	return (1);
 }
 
-int		main(int ac, char **av)
+int		main(int argc, char **argv)
 {
-	int 	fd;
-	char 	*line;
+	int		fd;
+	int		ret;
+	char	*line;
 
-	if (ac == 2)
+	argc = 0;
+	line = 0;
+	ret = 0;
+	fd = open(argv[1], O_RDONLY);
+	while ((ret = get_next_line(fd, &line)) > 0)
 	{
-		fd = open(av[1], O_RDONLY);
-		while (get_next_line (fd, &line) > 0)
-			printf("line = %s", line);
+		ft_putendl(line);
+		ft_strdel(&line);
 	}
-	else
-		printf("ERROR");
 	return (0);
 }
