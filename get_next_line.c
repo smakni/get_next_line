83aa26970ba5798279a7a71a58242b964535a97f
Sabri MAKNI <smakni@e3r10p5.42.fr>
2018-05-30 19:09:31 +0200

    add fd error

--- get_next_line.c
+++ get_next_line.c
@@ -6,7 +6,7 @@
 /*   By: smakni <smakni@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2018/05/25 11:15:06 by smakni            #+#    #+#             */
-/*   Updated: 2018/05/30 18:47:10 by smakni           ###   ########.fr       */
+/*   Updated: 2018/05/30 19:06:47 by smakni           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -27,9 +27,8 @@ int		get_next_line(const int fd, char **line)
 	int				ret;
 	char			buffer[BUFF_SIZE + 1];
 	static char		*tmp;
-	char			tmp_free;
 
-	if (!fd)
+	if (fd < 0)
 		return (-1);
 	if (!tmp)
 		tmp = ft_memalloc(1);
@@ -37,15 +36,10 @@ int		get_next_line(const int fd, char **line)
 	{
 		buffer[ret] = '\0';
 		tmp = ft_strjoin(tmp, buffer);
-		if (ft_memchr(buffer, '\n', ret) != 0 && ft_memchr(tmp, '\0', ret))
-			break ;
 	}
-	printf("tmp = %s\n", tmp);
 	*line = ft_memalloc(len_line(tmp) + 1);
 	*line = ft_strncpy(*line, (const char *)tmp, len_line(tmp));
 	tmp = ft_strsub(tmp, (len_line(tmp) + 1), (ft_strlen(tmp) - len_line(tmp)));
-	//printf("line = %s\n", *line);
-	//printf("tmp = %s\n", tmp);
 	if (ret < BUFF_SIZE && !ft_strlen(*line) && !ft_strlen(tmp))
 	{
 		ft_strdel(&(*line));
@@ -67,8 +61,8 @@ int		main(int argc, char **argv)
 	fd = open(argv[1], O_RDONLY);
 	while ((ret = get_next_line(fd, &line)) > 0)
 	{
-		//ft_putendl(line);
-		//ft_strdel(&line);
+		ft_putendl(line);
+		ft_strdel(&line);
 	}
 	return (0);
 }
